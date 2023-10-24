a62233e0e19b6fbe72a7f40942491fa81759f7c0
ayzana <aizanka001@gmail.com>
2023-10-24 17:35:58 +0300

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let result = '' ;
+  let num = Math.abs(n);
+  for (i = 0; i < 3; i++) {
+    result += num % 10;
+    num = Math.trunc(num / 10);
+   }
+   return result;
 }
